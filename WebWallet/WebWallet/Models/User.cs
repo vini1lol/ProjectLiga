a21aa0c1fe27512de49d1235dbd7e9114f74bed7
Vinícius dos Santos Tavares <47855624+vini1lol@users.noreply.github.com>
2021-08-10 13:37:11 -0300

    up

---
+++ WebWallet/WebWallet/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace WebWallet.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("E-mail")]
+        [Required]
+        public string Email { get; set; }
+        [DisplayName("Nome")]
+        [Required]
+        public string Name { get; set; }
+        [DisplayName("Senha")]
+        [Required]
+        public string Password { get; set; }
+    }
+}
